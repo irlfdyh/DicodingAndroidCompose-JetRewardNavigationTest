1d6bb9d40df1c508801445234863724702369d51
Irshal Firdeansyah <irshalfir@gmail.com>
2023-06-11 10:51:24 +0700

    prepare test setup before add test scenario

---
+++ app/src/androidTest/java/com/dicoding/jetreward/ui/screen/detail/DetailScreenKtTest.kt
@@ -0,0 +1,42 @@
+package com.dicoding.jetreward.ui.screen.detail
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
+import com.dicoding.jetreward.R
+import com.dicoding.jetreward.model.OrderReward
+import com.dicoding.jetreward.model.Reward
+import com.dicoding.jetreward.ui.theme.JetRewardTheme
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+
+class DetailScreenKtTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    private val fakeOrderReward = OrderReward(
+        reward = Reward(4, R.drawable.reward_4, "Jaket Hoodie Dicoding", 7500),
+        count = 0
+    )
+
+    @Before
+    fun setup() {
+        composeTestRule.setContent {
+            JetRewardTheme {
+                DetailContent(
+                    fakeOrderReward.reward.image,
+                    fakeOrderReward.reward.title,
+                    fakeOrderReward.reward.requiredPoint,
+                    fakeOrderReward.count,
+                    onBackClick = {},
+                    onAddToCart = {}
+                )
+            }
+        }
+        composeTestRule.onRoot().printToLog("currentLabelExist")
+    }
+
+}
